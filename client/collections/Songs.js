c7ef89261a9e651e884ebe16fd716baeb20dfc11
NicholasBelow <nkbelow@gmail.com>
2017-02-21 22:34:58 -0800

    App works as it should except will not fetch songs correctly

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,18 +1,18 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  url: 'http://parse.sfm8.hackreactor.com/mytunes/classes/songs',
+  url: 'http://parse.CAMPUS.hackreactor.com/mytunes/classes/songs',
 
-  parse: function(response) {
-    return response.results;
-  },
+  // parse: function(response) {
+  //   return response.results;
+  // },
 
   initialize: function() {
     this.fetch({
-      success: function(collection, response, options) {
+      success: (function(collection, response, options) {
         this.set(response.results);
         console.log('this workds');
-      }.bind(this)
+      }).bind(this)
     });
   },
 
